cd0a53f14e77b691bee471a31c6df1ae24708051
17214290014@academico.iesb.br <17214290014@academico.iesb.br>
2019-09-02 21:13:53 -0300

    Atividade05

---
+++ Atividade05/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
+  { path: 'home', loadChildren: './home/home.modulee#HomePageModule' },  { path: 'detalhes', loadChildren: './detalhes/detalhes.module#DetalhesPageModule' },
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
