075352b93eda4823e3d8fa3e4f401a954fd079a7
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2019-09-07 11:43:11 -0300

    Criando o projeto Atividade06

--- Atividade06/src/app/app-routing.module.ts
+++ Atividade06/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'opcoes', loadChildren: './opcoes/opcoes.module#OpcoesPageModule' },
+
 ];
 
 @NgModule({
