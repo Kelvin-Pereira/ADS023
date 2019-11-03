cc44c1ad1e1bc33705ccaebfac7914c910d777af
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2019-11-03 07:31:13 -0300

    ADD Atividade 15

---
+++ Atividade15/app/src/main/java/atividade15/a17214290014/atividade15/NovoRequerimentoActivity.kt
@@ -0,0 +1,12 @@
+package atividade15.a17214290014.atividade15
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NovoRequerimentoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_requerimento)
+    }
+}
